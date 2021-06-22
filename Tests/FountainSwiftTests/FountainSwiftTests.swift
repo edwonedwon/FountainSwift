efc60982c92e06b23c8d75be91a52caba5fcd7ac
Edwon <edwonia@gmail.com>
2021-06-23 08:24:14 +0900

    removed example tests

--- Tests/FountainSwiftTests/FountainSwiftTests.swift
+++ Tests/FountainSwiftTests/FountainSwiftTests.swift
@@ -22,42 +22,4 @@
                 .dialogue(Dialogue(character: "STEEL", dialogue: "Beer's ready!"))
             ])
         }
-        
-//        func testParsing_emptyDocument_shouldReturnNone() {
-//            let text = ""
-//            let parser = MarkdownParser(text)
-//            let nodes = parser.parse()
-//            XCTAssertEqual(nodes, [])
-//        }
-        
-//        func testParsing_singleLineOfText_shouldReturnSingleTextNode() {
-//            let text = "Hello World"
-//            let parser = MarkdownParser(text)
-//            let nodes = parser.parse()
-//            XCTAssertEqual(nodes, [
-//                .text("Hello World")
-//            ])
-//        }
-//
-//        func testParsing_multipleTextBlocksWithNestedBold_shouldReturnMultipleParagraphs() {
-//            let text = """
-//            This is a text block **with some bold text**.
-//
-//            Another paragraph with more **BOLD** text.
-//            """
-//            let parser = MarkdownParser(text)
-//            let nodes = parser.parse()
-//            XCTAssertEqual(nodes, [
-//                .paragraph(nodes: [
-//                    .text("This is a text block "),
-//                    .bold("with some bold text"),
-//                    .text(".")
-//                ]),
-//                .paragraph(nodes: [
-//                    .text("Another paragraph wtih more "),
-//                    .bold("BOLD"),
-//                    .text(" text."),
-//                ])
-//            ])
-//        }
     }
