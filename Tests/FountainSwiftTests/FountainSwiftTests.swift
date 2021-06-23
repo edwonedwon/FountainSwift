6b73046c9275b0c2d97e6d1925fb89067a0ba89b
Edwon <edwonia@gmail.com>
2021-06-23 19:04:09 +0900

    deleted unused tests

--- Tests/FountainSwiftTests/FountainSwiftTests.swift
+++ Tests/FountainSwiftTests/FountainSwiftTests.swift
@@ -2,7 +2,6 @@
     @testable import FountainSwift
 
     final class FountainSwiftTests: XCTestCase {
-        
         func testParsing_sceneHeading_action() {
             let text = """
             INT. HOUSE - DAY
@@ -99,84 +98,4 @@
                 .dialogue("Point Blank Sniper?"),
             ])
         }
-        
-//        func testParsing_simple() {
-//            let text = """
-//            EXT. BRICK'S PATIO - DAY
-//
-//            A gorgeous day.  The sun is shining.  But BRICK BRADDOCK, retired police detective, is sitting quietly, contemplating -- something.
-//
-//            The SCREEN DOOR slides open and DICK STEEL, his former partner and fellow retiree, emerges with two cold beers.
-//
-//            STEEL
-//            Beer's ready!
-//            """
-//            let parser = FountainParser(text)
-//            let nodes = parser.parse()
-//            XCTAssertEqual(nodes, [
-//                .sceneHeading("EXT. BRICK'S PATIO - DAY"),
-//                .action("A gorgeous day.  The sun is shining.  But BRICK BRADDOCK, retired police detective, is sitting quietly, contemplating -- something."),
-//                .action("The SCREEN DOOR slides open and DICK STEEL, his former partner and fellow retiree, emerges with two cold beers."),
-//                .dialogue(Dialogue(character: "STEEL", dialogue: "Beer's ready!"))
-//            ])
-//        }
-        
-//        func testParsing_section() {
-//            let text = """
-//            # This is a section
-//            """
-//            let parser = FountainParser(text)
-//            let nodes = parser.parse()
-//            XCTAssertEqual(nodes, [
-//                .
-//            ])
-//        }
-//
-//        func testParsing_everything() {
-//            let text = """
-//            # This is a section
-//
-//            = this is a synopses (which is basically a note, but could be used to summarize a scene)
-//
-//            EXT. HOUSE
-//
-//            A gorgeous day.  The sun is shining.
-//
-//            The SCREEN DOOR slides open.
-//
-//            !SCANNING THE AISLES...
-//            Where is that pit boss?
-//
-//            No luck. He has no choice but to deal the cards.
-//
-//            ## This is a section within a section
-//
-//            JIMMY
-//            Yo!
-//
-//            BRICK
-//            Sup?
-//
-//            STEEL
-//            They're coming out of the woodwork!
-//            (pause)
-//            No, everybody we've put away!
-//            (pause)
-//            Point Blank Sniper?
-//
-//            ===
-//
-//            STEEL
-//            (beer raised)
-//            To retirement.
-//            """
-//            let parser = FountainParser(text)
-//            let nodes = parser.parse()
-//            XCTAssertEqual(nodes, [
-//                .sceneHeading("EXT. BRICK'S PATIO - DAY"),
-//                .action("A gorgeous day.  The sun is shining.  But BRICK BRADDOCK, retired police detective, is sitting quietly, contemplating -- something."),
-//                .action("The SCREEN DOOR slides open and DICK STEEL, his former partner and fellow retiree, emerges with two cold beers."),
-//                .dialogue(Dialogue(character: "STEEL", dialogue: "Beer's ready!"))
-//            ])
-//        }
     }
